ae6eef6a30cdbb981ca1e605be7ad1c1647f784b
Ajit Singh <ajitsinghoriginal@gmail.com>
2025-05-06 22:43:55 +0530

    completed section 7

---
+++ Section-7/Definition.js
@@ -0,0 +1,21 @@
+/*
+Recursion is a process (a function in our case) that calls itself.
+
+It's everywhere
+JSON.parse
+JSON.stringify
+document.getElementById and DOM traversal algorithms
+Object traversal
+Cleaner alternative to loops/iteration
+
+
+
+How it works
+Two main components:
+
+DIFFERENT INPUT: Invoke the same function with a different input until a base case is reached
+
+BASE CASE : The condition under which the recursion ends.
+
+
+*/
---
+++ Section-7/The-Call-Stack.js
@@ -0,0 +1,6 @@
+/*
+It's a stack data structure
+Any time a function is invoked it is placed(pushed) on top of the call stack
+When javascript sees the return keyword or when the function ends,
+the compiler will remove(pop)
+*/
